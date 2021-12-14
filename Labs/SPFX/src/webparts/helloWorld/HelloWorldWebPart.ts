040a72f5936419b16f244566d8bdc84f18074415
vikash-verma-profile <er.vikashverma@yahoo.com>
2021-12-15 01:21:28 +0530

    lab on textproperties

--- Labs/SPFX/src/webparts/helloWorld/HelloWorldWebPart.ts
+++ Labs/SPFX/src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -78,6 +78,9 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
     return Version.parse('1.0');
   }
 
+  protected get disableReactivePropertyChanges(): boolean {
+      return true;
+  }
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
     return {
       pages: [
