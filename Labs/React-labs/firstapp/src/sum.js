05c6f98bdad2e7bdc19b4f9d3e3a57405c976d79
vikash-verma-profile <er.vikashverma@yahoo.com>
2021-12-16 23:10:00 +0530

    first commit

--- Labs/React-labs/firstapp/src/sum.js
+++ Labs/React-labs/firstapp/src/sum.js
@@ -6,25 +6,27 @@ class Sum  extends React.Component{
         this.state={
             Number1:0,
             Number2:0,
-            sum:0
+            Sum:0
         }
     }
-
     handleUserInput = (e) => {
         const name = e.target.name;
         const value = e.target.value;
         this.setState({ [name]: value });
     }
     Add=()=>{
-        console.log(this.state.Number1);
-        console.log(this.state.Number2);
+        var _sum=Number(this.state.Number1)+Number(this.state.Number2)
+       this.setState({Sum:_sum})
     }
     render(){
+        const {number1,number2}=this.state;
         return (
             <div >
                 <input type="text" name="Number1" value={this.state.Number1} onChange={this.handleUserInput} />
                 <input type="text" name="Number2" value={this.state.Number2} onChange={this.handleUserInput} />
                 <input type="button" value="SUM" onClick={this.Add}/>
+
+                <div>{this.state.Sum}</div>
             </div>
         );
     }
